0e385a96154561886a603e4506ebb92a025c4044
Derek D. Miller <derek.miller@arm.com>
2022-11-15 09:29:50 -0600

    path

--- vts/plugins/scheme-psa-iot/main.go
+++ vts/plugins/scheme-psa-iot/main.go
@@ -369,7 +369,7 @@ func MustInstIDString(c psatoken.IClaims) string {
 }
 
 func main() {
-	f, err := os.OpenFile("scheme-psa-iot.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("/work/veracruz/scheme-psa-iot.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
