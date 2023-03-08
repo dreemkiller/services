78dd91d89b13523a9b4e2a0eebf5323ec6ff73cc
Derek Miller <derek.miller@arm.com>
2023-03-08 17:13:58 +0000

    Forgot the scheme generated file

--- builtin/schemes.gen.go
+++ builtin/schemes.gen.go
@@ -4,17 +4,22 @@ import (
 	"github.com/veraison/services/plugin"
 
 	scheme1 "github.com/veraison/services/scheme/cca-ssd-platform"
-	scheme4 "github.com/veraison/services/scheme/psa-iot"
-	scheme2 "github.com/veraison/services/scheme/tcg-dice"
-	scheme3 "github.com/veraison/services/scheme/tpm-enacttrust"
+	scheme2 "github.com/veraison/services/scheme/psa-iot"
+	scheme3 "github.com/veraison/services/scheme/tcg-dice"
+	scheme4 "github.com/veraison/services/scheme/tpm-enacttrust"
+	scheme5 "github.com/veraison/services/scheme/aws-nitro"
+
 )
 
 var plugins = []plugin.IPluggable{
-	&scheme1.EvidenceHandler{},
 	&scheme1.EndorsementHandler{},
+	&scheme1.EvidenceHandler{},
+	&scheme2.EndorsementHandler{},
 	&scheme2.EvidenceHandler{},
 	&scheme3.EvidenceHandler{},
-	&scheme3.EndorsementHandler{},
-	&scheme4.EvidenceHandler{},
 	&scheme4.EndorsementHandler{},
+	&scheme4.EvidenceHandler{},
+	&scheme5.EndorsementHandler{},
+	&scheme5.EvidenceHandler{},
 }
+
